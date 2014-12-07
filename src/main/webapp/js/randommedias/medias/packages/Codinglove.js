52e5f17e3ab3ea67d199322e50f1e472779a7ad6
NargiT <nargit@msn.com>
2014-12-07 16:37:30 +0100

    Rename and change timer
    Rename CodeLoving to Codinglove
    change timer to 30 sec

--- src/main/webapp/js/randommedias/medias/packages/Codinglove.js
+++ src/main/webapp/js/randommedias/medias/packages/Codinglove.js
@@ -1,14 +1,14 @@
 /**
  * Created by nargit on 03/12/2014.
  */
-function Codeloving() {
-	var parameters = ["the code loving", "http://api.tumblr.com/v2/blog/thecodinglove.tumblr.com/posts?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4"];
+function Codinglove() {
+	var parameters = ["the coding love", "http://api.tumblr.com/v2/blog/thecodinglove.tumblr.com/posts?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4"];
 	Media.apply(this, parameters);
 }
 
-Codeloving.prototype = Object.create(Media.prototype);
-Codeloving.prototype.constructor = Codeloving;
-Codeloving.prototype.updateMedia = function (data) {
+Codinglove.prototype = Object.create(Media.prototype);
+Codinglove.prototype.constructor = Codinglove;
+Codinglove.prototype.updateMedia = function (data) {
 	var index = Math.floor(Math.random() * data.response.posts.length);
 	var regEx = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
 
