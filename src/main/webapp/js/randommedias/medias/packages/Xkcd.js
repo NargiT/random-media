932838d60cd72e9a0e50c960a0930784deee422a
NargiT <tigran.tchougourian@gmail.com>
2014-12-07 01:06:36 +0100

    Optimize xkcd implementation
    remove the pub/sub from the xkcd implementation

--- src/main/webapp/js/randommedias/medias/packages/Xkcd.js
+++ src/main/webapp/js/randommedias/medias/packages/Xkcd.js
@@ -12,20 +12,16 @@ Xkcd.prototype.updateMedia = function (data) {
 	var num = Math.floor(Math.random() * latestPostNum - 1); // to not pull to old images
 	var url = "http://dynamic.xkcd.com/api-0/jsonp/comic/" + num;
 
-
-	var controller = $(this);
-	controller.on("/Xkcd/internal/newImage", function (event, media) {
-		controller[0]._currentMedia.title = media.title;
-		controller[0]._currentMedia.url = media.url;
-		controller[0].notify();
-	});
+	var self = this;
 	// process an asynchronous call to get a random image
 	$.ajax({
 		url: url,
 		dataType: "jsonp",
 		success: function (data, textStatus, jqXHR) {
 			var media = {title: data.title, url: data.img};
-			controller.trigger("/Xkcd/internal/newImage", [media]);
+			self._currentMedia.title = media.title;
+			self._currentMedia.url = media.url;
+			self.notify();
 		},
 		error: function (jqXHR, textStatus, errorThrown) {
 			console.log(this.name + " failed to get the image");
